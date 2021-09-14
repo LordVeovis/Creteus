2543ef0085915c6a348e5f411dd0a8c9ad96aea8
Sébastien RAULT <sebastien.rault@nerd.nintendo.com>
2021-09-14 15:11:45 +0200

    handle more errors

--- Creteus.Console/CertificateValidationCallback.cs
+++ Creteus.Console/CertificateValidationCallback.cs
@@ -7,32 +7,32 @@ using System.Text;
 
 namespace Kveer.Creteus.Console
 {
-	class CertificateValidationCallback
-	{
-		protected IDictionary<string, X509Certificate2> _certificates;
+    class CertificateValidationCallback
+    {
+        protected IDictionary<string, Tuple<X509Certificate2, string>> _certificates;
 
-		protected string _hostname;
+        protected string _hostname;
 
-		public CertificateValidationCallback(string hostname, IDictionary<string, X509Certificate2> certificates)
-		{
-			_hostname = hostname;
-			_certificates = certificates;
-		}
+        public CertificateValidationCallback(string hostname, IDictionary<string, Tuple<X509Certificate2, string>> certificates)
+        {
+            _hostname = hostname;
+            _certificates = certificates;
+        }
 
-		public bool ServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-		{
-			string key = _hostname;
+        public bool ServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            string key = _hostname;
 
-			if (certificate == null || chain == null)
-			{
-				_certificates.Add(key, null);
-				return false;
-			}
+            if (certificate == null || chain == null)
+            {
+                _certificates.Add(key, null);
+                return false;
+            }
 
-			_certificates.Add(key, new X509Certificate2(certificate.GetRawCertData()));
+            _certificates.Add(key, new Tuple<X509Certificate2, string> (new X509Certificate2(certificate.GetRawCertData()), null));
 
-			return true;
-		}
+            return true;
+        }
 
-	}
+    }
 }
--- Creteus.Console/Program.cs
+++ Creteus.Console/Program.cs
@@ -1,103 +1,119 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
+using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Kveer.Creteus.Console
 {
-	class JsonResponse
-	{
-		public IList<string> sites { get; set; }
+    class JsonResponse
+    {
+        public IList<string> sites { get; set; }
 
-		public int warnDays { get; set; }
-	}
+        public int warnDays { get; set; }
+    }
 
-	class Program
-	{
-		private static IDictionary<string, X509Certificate2> _certificates = new Dictionary<string, X509Certificate2>();
+    class Program
+    {
+        private static IDictionary<string, Tuple<X509Certificate2, string>> _certificates = new Dictionary<string, Tuple<X509Certificate2, string>>();
 
-		static void Main(string[] args)
-		{
-			var json = JsonConvert.DeserializeObject<JsonResponse>(File.ReadAllText("sites.json"));
-			var sites = json.sites;
+        static void Main(string[] args)
+        {
+            var json = JsonConvert.DeserializeObject<JsonResponse>(File.ReadAllText("sites.json"));
+            var sites = json.sites;
 
-			//ServicePointManager.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
-			foreach (var s in sites)
-			{
-				Uri url;
-				var urlResult = Uri.TryCreate(s, UriKind.Absolute, out url);
+            //ServicePointManager.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
+            foreach (var s in sites)
+            {
+                var urlResult = Uri.TryCreate(s, UriKind.Absolute, out Uri url);
+                if (!urlResult)
+                {
+                    _certificates.Add(s, new Tuple<X509Certificate2, string>(null, "Invalid URL"));
+                    continue;
+                }
 
-				switch (url.Scheme)
-				{
-					case "https":
-						var request = (HttpWebRequest)WebRequest.Create(s);
-						request.ServerCertificateValidationCallback = new CertificateValidationCallback(url.DnsSafeHost, _certificates).ServerCertificateValidationCallback;
-						request.AllowAutoRedirect = false;
+                Debug.WriteLine($"Testing {url.DnsSafeHost}");
 
-						try
-						{
-							using (var response = request.GetResponse())
-								response.Close();
-						}
-						catch (WebException e) when ((e.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.Found)
-						{
+                switch (url.Scheme)
+                {
+                    case "https":
+                        var request = (HttpWebRequest)WebRequest.Create(s);
+                        request.ServerCertificateValidationCallback = new CertificateValidationCallback(url.DnsSafeHost, _certificates).ServerCertificateValidationCallback;
+                        request.AllowAutoRedirect = false;
 
-						}
-						break;
-					case "smtps":
-						using (var smtpClient = new SmtpClient(url.DnsSafeHost, url.Port))
-						{
-							ServicePointManager.ServerCertificateValidationCallback = new CertificateValidationCallback(url.DnsSafeHost, _certificates).ServerCertificateValidationCallback; 
-							smtpClient.EnableSsl = true;
-							smtpClient.UseDefaultCredentials = false;
-							using (var mail = new MailMessage("test@example.nono", "test@example.nono", "Test", "Test"))
-								try
-								{
-									smtpClient.Send(mail);
-								}
-								catch (SmtpFailedRecipientException e)
-								{
+                        try
+                        {
+                            using (var response = request.GetResponse())
+                                response.Close();
+                        }
+                        catch (WebException e) when ((e.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.Found)
+                        {
 
-								}
-						}
+                        }
+                        catch (WebException e) when ((e.InnerException?.InnerException as SocketException)?.SocketErrorCode == SocketError.HostNotFound || (e.InnerException?.InnerException as SocketException)?.SocketErrorCode == SocketError.TimedOut)
+                        {
+                            _certificates.Add(url.DnsSafeHost, new Tuple<X509Certificate2, string>(null, e.InnerException.Message));
+                        }
+                        break;
+                    case "smtps":
+                        using (var smtpClient = new SmtpClient(url.DnsSafeHost, url.Port))
+                        {
+                            ServicePointManager.ServerCertificateValidationCallback = new CertificateValidationCallback(url.DnsSafeHost, _certificates).ServerCertificateValidationCallback;
+                            smtpClient.EnableSsl = true;
+                            smtpClient.UseDefaultCredentials = false;
+                            using var mail = new MailMessage("test@example.nono", "test@example.nono", "Test", "Test");
+                            try
+                            {
+                                smtpClient.Send(mail);
+                            }
+                            catch (SmtpFailedRecipientException e)
+                            {
 
-						ServicePointManager.ServerCertificateValidationCallback = null;
-						break;
-					default:
-						_certificates.Add(s, null);
-						break;
-				}
+                            }
+                        }
 
-			}
+                        ServicePointManager.ServerCertificateValidationCallback = null;
+                        break;
+                    default:
+                        _certificates.Add(s, null);
+                        break;
+                }
 
-			foreach (var el in _certificates)
-			{
-				var url = el.Key;
-				System.Console.Write($"{url}: ");
+            }
 
-				if (el.Value == null)
-				{
-					ConsoleExtension.Write("ERROR: NO CERTIFICATE", ConsoleColor.DarkRed);
-					continue;
-				}
+            foreach (var el in _certificates)
+            {
+                var url = el.Key;
+                System.Console.Write($"{url}: ");
 
-				var today = DateTime.Today;
-				var daysBeforeExpiration = Math.Floor((el.Value.NotAfter - today).TotalDays);
+                if (!string.IsNullOrEmpty(el.Value?.Item2))
+                {
+                    ConsoleExtension.WriteLine($"ERROR: {el.Value.Item2}", ConsoleColor.DarkRed);
+                    continue;
+                }
+                else if (el.Value?.Item1 == null)
+                {
+                    ConsoleExtension.WriteLine("ERROR: NO CERTIFICATE", ConsoleColor.DarkRed);
+                    continue;
+                }
 
+                var today = DateTime.Today;
+                var daysBeforeExpiration = Math.Floor((el.Value.Item1.NotAfter - today).TotalDays);
 
-				if (daysBeforeExpiration < json.warnDays)
-					ConsoleExtension.Write("MUST BE REPLACED ", ConsoleColor.Red);
-				else
-					ConsoleExtension.Write("OK ", ConsoleColor.Green);
-				System.Console.WriteLine($" ({daysBeforeExpiration} days remaining)");
-			}
+                if (daysBeforeExpiration < json.warnDays)
+                    ConsoleExtension.Write("MUST BE REPLACED ", ConsoleColor.Red);
+                else
+                    ConsoleExtension.Write("OK ", ConsoleColor.Green);
+                System.Console.WriteLine($" ({daysBeforeExpiration} days remaining)");
+            }
 
-			if (!System.Diagnostics.Debugger.IsAttached)
-				System.Console.ReadLine();
-		}
-	}
+            if (!System.Diagnostics.Debugger.IsAttached)
+                System.Console.ReadLine();
+        }
+    }
 }
